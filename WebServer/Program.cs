56f8a03b7da5da37414c1e8615e091cad4a356ca
whypikmi <ttelatnikova1@yandex.ru>
2026-04-10 10:58:41 +0300

    Step 8: middleware logging and custom headers

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -1,7 +1,31 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"[LOG] {context.Request.Method} {context.Request.Path}");
+    await next(context);
+    Console.WriteLine($"[LOG] Ответ отправлен: {context.Response.StatusCode} ");
+});
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Append("X-Powered-By", "ASP.NET Core Lab27");
+    await next(context);
+});
+app.Use(async (context, next) =>
+{
+    var key = context.Request.Query["key"];
+    if (key != "secret")
+    {
+        context.Response.StatusCode = 401;
+        await context.Response.WriteAsync("Код ошибки: 401 Unauthorized");
+        Console.WriteLine($"[LOG] Ответ отправлен: {context.Response.StatusCode}");
+        return;
+    }
 
-// app.MapGet("/", () => "Привет от ИСП-231! Авторы: Лиза и Настя");
+    Console.WriteLine("[LOG] Все норм, едем дальше");
+    await next(context);
+});
+app.MapGet("/", () => "Привет от ИСП-231! Авторы: Лиза и Настя");
 // app.MapGet("/", () => "Добро пожаловать на сервер!");
 // app.MapGet("/about", () => "Это мой первый ASP.NET Core сервер");
 // app.MapGet("/time", () => $"Время на сервере: {DateTime.Now}");
@@ -21,11 +45,11 @@ var app = builder.Build();
 //     "ИСРПО",
 //     "СП",
 // });
-app.MapGet("/product/{id}", (int id) => new Product(
-    Id: id,
-    Name: $"Товар #{id}",
-    Price: id * 99.99m,
-    InStock: id % 2 == 0
-));
+// app.MapGet("/product/{id}", (int id) => new Product(
+//     Id: id,
+//     Name: $"Товар #{id}",
+//     Price: id * 99.99m,
+//     InStock: id % 2 == 0
+// ));
 app.Run();
 record Product(int Id, string Name, decimal Price, bool InStock);
